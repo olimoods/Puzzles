9290a44fcf48e65f45e4bbd5e01ad0c99b4758d1
haydn <haydn_davies@student.wayland.k12.ma.us>
2017-10-26 21:41:37 -0400

    put in the text

--- src/Main.java
+++ src/Main.java
@@ -42,30 +42,30 @@ public class Main {
     public Main() {
         yourRoom = new Event(null, null, "yee");
 
-        dresser = new Event("text");
+        dresser = new Event("You pull the door but it seems to be locked. There is a muffled sound coming from inside…", "dresser");
 
-        bathroom = new Event("Text");
-        bathroomShower = new Event("Text");
-        bathroomToilet = new Event("text");
-        bathroomToiletBowl = new Event("Text");
-        bathroomToiletBack = new Event("Text");
-        bathroomToiletFlush = new Event("text");
+        bathroom = new Event("Door “2” is locked. Maybe if I had something to pick the lock it will open.", "bathroom");
+        bathroomShower = new Event("You go to the shower, reach down and try the knob, but it is stuck, so you twist it hard and it turns on. But you are immediately engulfed in fire and die.", "bathroom");
+        bathroomToilet = new Event("You see the out of place toilet because it looks brand new. You go over to it", "bathroom");
+        bathroomToiletBowl = new Event("You look in the toilet bowl, and see a flashlight and a goldfish", "bathroom");
+        bathroomToiletBack = new Event("You look in the back of the toilet and see a large brick, it is somehow not eroded at all and very strong.", "bathroom");
+        bathroomToiletFlush = new Event("You flush the toilet to see what it does, bad call. You hear the door shut with a metallic click. You notice the toilet is not flushing, but it is filling up and flooding the room very fast. You run to the door to try to get out, but as you thought it is locked. Use the brick to escape", "bathroom");
         bathroomToilet.addEvent(bathroomToiletBack);
         bathroomToilet.addEvent(bathroomToiletBowl);
         bathroomToilet.addEvent(bathroomToiletFlush);
-        bathroomCabinet = new Event("text");
+        bathroomCabinet = new Event("While in the bathroom, you see a cabinet, the hinges are rusted over, and there is a hole in one of the doors. When you open the door you see two items covered in cobwebs. A blacklight, and bullets.", "bathroom");
         bathroom.addEvent(bathroomShower);
         bathroom.addEvent(bathroomToilet);
         bathroom.addEvent(bathroomCabinet);
 
-        kitchen = new Event("Text");
-        kitchenSink = new Event("text");
-        kitchenSinkWater = new Event("text");
-        kitchenSinkDrain = new Event("text");
+        kitchen = new Event("You open door 1 to find a small kitchen. You see a sink, a Dishwasher, and a microwave.", "kitchen");
+        kitchenSink = new Event("The sink looks rusty, maybe the water still works.", "kitchen");
+        kitchenSinkWater = new Event("You turn the knob and a few drops come out along with a clothespin.", "kitchen");
+        kitchenSinkDrain = new Event("You look down the rusty drain, there is nothing to be found.", "kitchen");
         kitchenSink.addEvent(kitchenSinkWater);
         kitchenSink.addEvent(kitchenSinkDrain);
-        kitchenMicrowave = new Event("text");
-        kitchenDishwasher = new Event("text");
+        kitchenMicrowave = new Event("You try to open the microwave but it won’t budge. The display seems to be flashing something… it says “enter code”", "kitchen");
+        kitchenDishwasher = new Event("You pull open the dishwasher. It clearly hasn’t been run in a while, there are cobwebs everywhere along with a couple objects: a Knife, a bagel, and an oven mitt.", "kitchen");
         kitchen.addEvent(kitchenSink);
         kitchen.addEvent(kitchenMicrowave);
         kitchen.addEvent(kitchenDishwasher);
@@ -75,7 +75,7 @@ public class Main {
         startingRoom.addEvent(kitchen);
         startingRoom.addEvent(bathroom);
         startingRoom.addEvent(livingRoom);
-        startingRoom = new Event("*Thunder Claps* *Lightning flashes* You wake up in a daze. Where am I? What is this place? It appears to be a dark room with high vaulted ceilings. There are three doors, each labeled 1-3. The room is empty except for a large dresser in the corner.", "start");
+        startingRoom = new Event("*Thunder Claps* *Lightning flashes* You wake up in a daze. Where am I? What is this place? It appears to be a dark room with high vaulted ceilings. There are three doors, each labeled 1-3. The room is empty except for a large dresser in the corner. You reach into your pocket and pull out a small piece of paper, written on it in chicken scratch handwriting ‘KCIUQ TUO TEG’", "start");
         yourRoom = startingRoom;
         yourAction = new Actions();
     }
