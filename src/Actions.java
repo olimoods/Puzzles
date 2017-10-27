a3bd77cecd65191443ad19887fddbd80864d54e3
TBrient <tylerbrient@gmail.com>
2017-10-27 09:25:15 -0400

    added more to the nerd

--- src/Actions.java
+++ src/Actions.java
@@ -26,6 +26,11 @@ public class Actions {
 //        userInventory.addObject("flushed");
     }
 
+    public static String getInput() {
+        Scanner s = new Scanner(System.in);
+        return s.nextLine().toLowerCase();
+    }
+
     public int callOption(String userInput, Event room) {
         String[] input = userInput.split(" ");
         String action = input[0];
@@ -62,28 +67,42 @@ public class Actions {
                             }
 
                             if (thing.equalsIgnoreCase("nerd")) {
-                                System.out.println("You get closer to the nerd. You can either 'run away', 'talk' to him, or 'beat' him up.");
-                                String userInputNerd = getInput();
-                                if (userInputNerd.equalsIgnoreCase("run away")) {
-                                    System.out.println("You run straight into the wall, killing yourself instantly");
-                                    return -3;
-                                } else if (userInputNerd.equalsIgnoreCase("talk")) {
-                                    System.out.println("The nerd gives you a piece of paper with a code on it");
-                                    userInventory.addObject("Code");
-                                    System.out.println("You can either 'lock him up' or 'let him go'");
-                                    String userInputNerd2 = getInput();
-                                    if (userInputNerd2.equalsIgnoreCase("lock him up")) {
-                                        System.out.println("You lock the nerd up, and close the dresser");
+                                if (userInventory.isObjectInInventory("Incorrect Code")) {
+                                    System.out.println("You approach the nerd again, you can either 'talk' to him, or 'beat' him up.");
+                                    String userInputNerd = getInput();
+                                    if (userInputNerd.equalsIgnoreCase("talk")) {
+                                        System.out.println("You tell the nerd that the code didn't work, he snickers, and curls up, never to speak again.");
                                         return -3;
-                                    } else if (userInputNerd2.equalsIgnoreCase("let him go")) {
-                                        System.out.println("The nerd sprints away, you chase after him, but run straight into a wall, killing yourself instantly.");
+                                    } else if (userInputNerd.equalsIgnoreCase("beat")) {
+                                        System.out.println("The nerd whimpers as you beat him into the corner of the dresser. Finally, he hands you a second sheet of paper, this one with a different code");
+                                        userInventory.addObject("Code");
                                         return -3;
                                     }
-                                } else if (userInputNerd.equalsIgnoreCase("beat")) {
-                                    System.out.println("The nerd curls up, silent. He's used to this.");
-                                    return -3;
+                                } else {
+                                    System.out.println("You get closer to the nerd. You can either 'run away', 'talk' to him, or 'beat' him up.");
+                                    String userInputNerd = getInput();
+                                    if (userInputNerd.equalsIgnoreCase("run away")) {
+                                        System.out.println("You run straight into the wall, killing yourself instantly");
+                                        return -3;
+                                    } else if (userInputNerd.equalsIgnoreCase("talk")) {
+                                        System.out.println("The nerd gives you a piece of paper with a code on it");
+                                        userInventory.addObject("Incorrect Code");
+                                        System.out.println("You can either 'lock him up' or 'let him go'");
+                                        String userInputNerd2 = getInput();
+                                        if (userInputNerd2.equalsIgnoreCase("lock him up")) {
+                                            System.out.println("You lock the nerd up, and close the dresser");
+                                            return -3;
+                                        } else if (userInputNerd2.equalsIgnoreCase("let him go")) {
+                                            System.out.println("The nerd sprints away, you chase after him, but run straight into a wall, killing yourself instantly.");
+                                            return -3;
+                                        }
+                                    } else if (userInputNerd.equalsIgnoreCase("beat")) {
+                                        System.out.println("The nerd curls up, silent. He's used to this.");
+                                        return -3;
+                                    }
                                 }
                             }
+
 
                             if (thing.equalsIgnoreCase("basement") && room.getName().equalsIgnoreCase("stairs")){
                                 System.out.println("You fall down a hole in the stairs into a well and die");
@@ -96,11 +115,6 @@ public class Actions {
             }
         }
         return -2;
-    }
-
-    public static String getInput() {
-        Scanner s = new Scanner(System.in);
-        return s.nextLine().toLowerCase();
     }
 
     public Event makeConnection(Event room, int index) {
